840735b72a92be37e370decc66b974ba19a29ce0
Timothy Santos <tsantos@stanford.edu>
2014-02-12 13:29:31 +0000

    putting it together lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,9 +25,21 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	$.get("/project/" + idNumber, function(result) {
+		if(result) {
+			console.log(result);
+			var projectHTML = '<a href="#" class="thumbnail">' +
+			'<img src="' + result['image'] + '" class="detailsImage">' +
+			'<p>' + result['title'] + '</p>' + 
+			'<p><small>' + result['date'] + '</small></p></a>' + result['summary']; 
+			$('#' + projectID + ' .details').html(projectHTML);
+		}
+	})
 
 	console.log("User clicked on project " + idNumber);
 }
+
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +47,13 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", function(result) {
+		console.log(result['colors']['hex']);
+		var colors = result['colors']['hex'];
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	})
 }
